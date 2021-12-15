41a3b7c155437e3c11a4aab81d03d2222f7b5e20
RademakerJ <j.rademaker@live.nl>
2021-12-15 22:36:23 +0100

    Day 3 cleanup.

--- AdventOfCode2021/Day3/Program.ReadInput.cs
+++ AdventOfCode2021/Day3/Program.ReadInput.cs
@@ -2,11 +2,31 @@
 {
     public partial class Program
     {
-        private static partial IReadOnlyList<string> ReadInput()
+        private static partial IReadOnlyList<IReadOnlyList<int>> ReadInput()
         {
             var filePath = Path.Combine(Path.GetFullPath(@"..\..\..\"), "Resources", "Input.txt");
+            var lines = File.ReadAllLines(filePath);
 
-            return File.ReadAllLines(filePath);
+            return GetMatrix(lines);
+        }
+
+        private static IReadOnlyList<IReadOnlyList<int>> GetMatrix(IReadOnlyList<string> bits)
+        {
+            var matrix = new List<List<int>>();
+
+            foreach (var bit in bits)
+            {
+                var row = new List<int>();
+
+                foreach (char c in bit)
+                {
+                    row.Add(int.Parse(c.ToString()));
+                }
+
+                matrix.Add(row);
+            }
+
+            return matrix;
         }
     }
 }
--- AdventOfCode2021/Day3/Program.cs
+++ AdventOfCode2021/Day3/Program.cs
@@ -2,130 +2,100 @@
 {
     public partial class Program
     {
-        private static partial IReadOnlyList<string> ReadInput();
+        private static partial IReadOnlyList<IReadOnlyList<int>> ReadInput();
 
         public static void Main(string[] _)
         {
-            var bits = ReadInput();
+            var bitsMatrix = ReadInput();
 
-            Console.WriteLine($"Solution to problem A: {SolveProblemA(bits)}");     // 3009600
-            Console.WriteLine($"Solution to problem B: {SolveProblemB(bits)}");     // 6940518
+            Console.WriteLine($"Solution to problem A: {SolveProblemA(bitsMatrix)}");     // 3009600
+            Console.WriteLine($"Solution to problem B: {SolveProblemB(bitsMatrix)}");     // 6940518
         }
 
-        private static int SolveProblemA(IReadOnlyList<string> bits)
+        /// <summary>
+        /// Power consumption.
+        /// </summary>
+        private static int SolveProblemA(IReadOnlyList<IReadOnlyList<int>> matrix)
         {
-            var matrix = GetMatrix(bits);
-
-            var (gammaRate, epsilonRate) = GetGammaAndEpsilonRate(matrix);
-
-            return gammaRate * epsilonRate;
-        }
-
-        private static int SolveProblemB(IReadOnlyList<string> bits)
-        {
-            var matrix = GetMatrix(bits);
-
-            var (oxygenRate, scrubberRate) = GetOxygenScrubberRate(matrix);
-
-            return oxygenRate * scrubberRate;
-        }
-
-        private static (int oxygenRate, int scrubberRate) GetOxygenScrubberRate(IReadOnlyList<IReadOnlyList<int>> matrix)
-        {
-            var oxygenSequence = GetBitSequence(matrix, 1, false);
-            var scrubberSequence = GetBitSequence(matrix, 0, true);
-
-            var oxygenSequenceStr = new string(oxygenSequence.Select(bit => bit.ToString()).SelectMany(bit => bit).ToArray());
-            var scrubberSequenceStr = new string(scrubberSequence.Select(bit => bit.ToString()).SelectMany(bit => bit).ToArray());
-
-            var oxygenRate = Convert.ToInt32(oxygenSequenceStr, 2);
-            var scrubberRate = Convert.ToInt32(scrubberSequenceStr, 2);
-
-            return (oxygenRate, scrubberRate);
-        }
-
-        private static IReadOnlyList<int> GetBitSequence(IReadOnlyList<IReadOnlyList<int>> matrix, int mostCommonBitChoice, bool leastCommon)
-        {
-            var matrixIndexToSkip = new List<int>();
+            var gammaBits = new List<int>();
 
             for (int i = 0; i < matrix[0].Count; i++)
             {
-                var column = matrix.Where((r, i) => !matrixIndexToSkip.Contains(i)).Select(row => row[i]);
+                var column = matrix.Select(row => row[i]).ToList();
 
-                var bitGrouping = column.GroupBy(bit => bit);
-                bitGrouping = leastCommon ? bitGrouping.OrderBy(group => group.Count()) : bitGrouping.OrderByDescending(group => group.Count());
+                gammaBits.Add(GetMostCommonBit(column, tieBreakerBit: 1));
+            }
 
-                var mostCommonBit = bitGrouping.Count() == 1 ? bitGrouping.First().Key :
-                    bitGrouping.First().Count() == bitGrouping.ElementAt(1).Count() ? mostCommonBitChoice :
-                    bitGrouping.First().Key;
+            var gamma = GetBitSequence(gammaBits);
+            var epsilon = GetBitSequence(gammaBits.Select(bit => bit == 1 ? 0 : 1).ToList());
+
+            return Convert.ToInt32(gamma, 2) * Convert.ToInt32(epsilon, 2);
+        }
+
+        /// <summary>
+        /// Life support rating.
+        /// </summary>
+        private static int SolveProblemB(IReadOnlyList<IReadOnlyList<int>> matrix)
+        {
+            var oxygenSequence = new string(
+                 GetBitSequence(GetMostCommonBits(matrix, tieBreakerBit: 1, takeLeastCommonBit: false)));
+
+            var scrubberSequence = new string(
+                GetBitSequence(GetMostCommonBits(matrix, tieBreakerBit: 0, takeLeastCommonBit: true)));
+
+            return Convert.ToInt32(oxygenSequence, 2) * Convert.ToInt32(scrubberSequence, 2);
+        }
+
+        private static IReadOnlyList<int> GetMostCommonBits(IReadOnlyList<IReadOnlyList<int>> matrix, int tieBreakerBit, bool takeLeastCommonBit)
+        {
+            var indicesToSkip = new List<int>();
+
+            for (int i = 0; i < matrix[0].Count; i++)
+            {
+                var column = matrix.Where((_, i) => !indicesToSkip.Contains(i)).Select(row => row[i]).ToList();
 
                 for (int j = 0; j < matrix.Count; j++)
                 {
-                    if (matrix[j][i] != mostCommonBit)
+                    if (matrix[j][i] != GetMostCommonBit(column, tieBreakerBit, !takeLeastCommonBit))
                     {
-                        matrixIndexToSkip.Add(j);
+                        indicesToSkip.Add(j);
                     }
                 }
             }
 
-            var bitsToSequence = new List<int>();
-            for (int j = 0; j < matrix.Count; j++)
+            for (int k = 0; k < matrix.Count; k++)
             {
-                if (!matrixIndexToSkip.Contains(j))
+                if (!indicesToSkip.Contains(k))
                 {
-                    bitsToSequence = matrix[j].ToList();
-                    break;
+                    return matrix[k].ToList();
                 }
             }
 
-            return bitsToSequence;
+            // We are just going to assume that this is not going to happen :^).
+            return Array.Empty<int>();
         }
 
-        private static (int gammaRate, int epsilonRate) GetGammaAndEpsilonRate(IReadOnlyList<IReadOnlyList<int>> matrix)
+        private static int GetMostCommonBit(IReadOnlyList<int> column, int tieBreakerBit, bool orderByDescending = false)
         {
-            var bits = new List<int>();
-            var gammaRate = 0;
-            var epsilonRate = 0;
+            var bitCountGroupings = column.GroupBy(bit => bit);
+            bitCountGroupings = orderByDescending ?
+                bitCountGroupings.OrderByDescending(group => group.Count()) :
+                bitCountGroupings.OrderBy(group => group.Count());
 
-            for (int i = 0; i < matrix[0].Count; i++)
-            {
-                var column = matrix.Select(row => row[i]);
-
-                var mostCommonBit = column
-                    .GroupBy(bit => bit)
-                    .OrderByDescending(group => group.Count())
-                    .Select(group => group.Key)
-                    .First();
-
-                bits.Add(mostCommonBit);
-            }
-
-            var bitSequence = new string(bits.Select(bit => bit.ToString()).SelectMany(bit => bit).ToArray());
-            var bitSequenceEpsilon = new string(bits.Select(bit => (bit == 1 ? 0 : 1).ToString()).SelectMany(bit => bit).ToArray());
-
-            gammaRate = Convert.ToInt32(bitSequence, 2);
-            epsilonRate = Convert.ToInt32(bitSequenceEpsilon, 2);
-
-            return (gammaRate, epsilonRate);
+            return bitCountGroupings.Count() == 1 ? bitCountGroupings.First().Key :
+                   bitCountGroupings.First().Count() == bitCountGroupings.ElementAt(1).Count() ? tieBreakerBit :
+                   bitCountGroupings.First().Key;
         }
 
-        private static IReadOnlyList<IReadOnlyList<int>> GetMatrix(IReadOnlyList<string> bits)
+        private static string GetBitSequence(IReadOnlyList<int> mostCommonBits)
         {
-            var matrix = new List<List<int>>();
+            var bitSequenceX = new string(
+                mostCommonBits
+                    .Select(bit => bit.ToString())
+                    .SelectMany(bit => bit)
+                    .ToArray());
 
-            foreach (var bit in bits)
-            {
-                var row = new List<int>();
-
-                foreach (char c in bit)
-                {
-                    row.Add(int.Parse(c.ToString()));
-                }
-
-                matrix.Add(row);
-            }
-
-            return matrix;
+            return bitSequenceX;
         }
     }
 }
