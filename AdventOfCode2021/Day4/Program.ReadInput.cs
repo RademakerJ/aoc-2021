401ddaebf1b6523249f21c9df2dd4677c364885a
RademakerJ <j.rademaker@live.nl>
2021-12-16 19:51:09 +0100

    Added day 4.

---
+++ AdventOfCode2021/Day4/Program.ReadInput.cs
@@ -0,0 +1,38 @@
+﻿namespace Day4
+{
+    public partial class Program
+    {
+        private static partial (IReadOnlyList<int>, List<List<List<int>>>) ReadInput()
+        {
+            var filePath = Path.Combine(Path.GetFullPath(@"..\..\..\"), "Resources", "Input.txt");
+
+            using StreamReader reader = new(filePath);
+            var bingoNumbers = reader.ReadLine()?.Split(',').Select(str => int.Parse(str)).ToList();
+            var bingoCardsRaw = reader.ReadToEnd()
+                .Split(new string[] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (bingoNumbers is null || bingoCardsRaw is null)
+            {
+                throw new Exception("This is not supposed to happen.");
+            }
+
+            return (bingoNumbers, GetBingoCards(bingoCardsRaw).ToList());
+        }
+
+        private static IEnumerable<List<List<int>>> GetBingoCards(IReadOnlyList<string> bingoCardsRaw)
+        {
+            foreach (var bingoCardRaw in bingoCardsRaw)
+            {
+                var bingoCard = new List<List<int>>();
+                var rowSplit = bingoCardRaw.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var row in rowSplit)
+                {
+                    bingoCard.Add(row.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(str => int.Parse(str.Trim())).ToList());
+                }
+
+                yield return bingoCard;
+            }
+        }
+    }
+}
---
+++ AdventOfCode2021/Day4/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace Day4
+{
+    public partial class Program
+    {
+        private static partial (IReadOnlyList<int>, List<List<List<int>>>) ReadInput();
+
+        public static void Main(string[] _)
+        {
+            var (bingoNumbers, bingoCards) = ReadInput();
+
+            Console.WriteLine($"Solution to problem A: {SolveProblem(bingoCards, bingoNumbers, 1)}");                   // 33348
+            Console.WriteLine($"Solution to problem B: {SolveProblem(bingoCards, bingoNumbers, bingoCards.Count)}");    // 8112
+        }
+
+        private static int SolveProblem(List<List<List<int>>> bingoCards, IReadOnlyList<int> bingoNumbers, int nthWinner)
+        {
+            // Since we are going to meddle with the bingo cards, make a copy of the original bingo cards list beforehand.
+            // (Otherwise we will get in trouble with problem B).
+            var bingoCardsCopy = new List<List<List<int>>>(bingoCards);
+
+            if (PlayBingo(bingoCardsCopy, bingoNumbers, nthWinner) is (List<List<int>> bingoCard, int bingoNumber))
+            {
+                return GetFinalScore(bingoCard, bingoNumber);
+            };
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Returns the nth winning card and the winning number.
+        /// </summary>
+        private static (List<List<int>>, int)? PlayBingo(List<List<List<int>>> bingoCards, IReadOnlyList<int> bingoNumbers, int nthWinner)
+        {
+            var lastWinningBingoCard = new List<List<int>>();
+            int lastWinningBingoNumber = 0;
+            var bingoCardsToSkip = new List<int>();
+            int numberOfWinners = 0;
+
+            foreach (var bingoNumber in bingoNumbers)
+            {
+                for (int i = 0; i < bingoCards.Count; i++)
+                {
+                    if (bingoCardsToSkip.Contains(i))
+                    {
+                        continue;
+                    }
+
+                    for (int row = 0; row < bingoCards[i].Count; row++)
+                    {
+                        for (int col = 0; col < bingoCards[i][row].Count; col++)
+                        {
+                            if (bingoCards[i][row][col] == bingoNumber)
+                            {
+                                bingoCards[i][row][col] = -1;
+
+                                if (Bingo(bingoCards[i], (row, col)))
+                                {
+                                    numberOfWinners++;
+                                    lastWinningBingoCard = bingoCards[i];
+                                    lastWinningBingoNumber = bingoNumber;
+                                    bingoCardsToSkip.Add(i);
+
+                                    if (numberOfWinners == nthWinner)
+                                    {
+                                        return (lastWinningBingoCard, lastWinningBingoNumber);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return (lastWinningBingoCard, lastWinningBingoNumber);
+        }
+
+        private static bool Bingo(List<List<int>> bingoCard, (int row, int col) position)
+        {
+            return
+                bingoCard[position.row].All(number => number == -1) ||
+                bingoCard.Select(row => row[position.col]).All(number => number == -1);
+        }
+
+        private static int GetFinalScore(List<List<int>> bingoCard, int winningNumber)
+        {
+            return bingoCard.SelectMany(row => row).Where(number => number != -1).Sum() * winningNumber;
+        }
+    }
+}
